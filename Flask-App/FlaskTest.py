61a746f8369a43eed553b0cbc38348dfefcc3f50
Kevin <Kevin@DESKTOP-12A507O>
2018-10-26 10:05:18 +0100

    Stream data test from server to front end template

--- Flask-App/FlaskTest.py
+++ Flask-App/FlaskTest.py
@@ -1,12 +1,28 @@
-from flask import Flask
+# Adapted from https://stackoverflow.com/questions/31948285/display-data-streamed-from-a-flask-view-as-it-updates
+
+from time import sleep
+
+from flask import Flask, render_template
+
+from math import sqrt
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    # render the template (below) that will use JavaScript to read the stream
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/stream_sqrt')
+def stream():
+    def generate():
+        for i in range(500):
+            yield '{}\n'.format(sqrt(i))
+            sleep(0.001)
+
+    return app.response_class(generate(), mimetype='text/plain')
+
+
+app.run()
