1c4430d80f5bd910f736aee1dab0006d5e5ca06d
Kevin <Kevin@DESKTOP-12A507O>
2018-11-06 16:10:47 +0000

    Stream data to javascript variable

--- Flask-App/FlaskTest.py
+++ Flask-App/FlaskTest.py
@@ -1,5 +1,34 @@
-# Adapted from https://stackoverflow.com/questions/31948285/display-data-streamed-from-a-flask-view-as-it-updates
 
+#!/usr/bin/env python
+import time
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+
+def stream_template(template_name, **context):
+    # http://flask.pocoo.org/docs/patterns/streaming/#streaming-from-templates
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    # uncomment if you don't need immediate reaction
+    ##rv.enable_buffering(5)
+    return rv
+
+
+@app.route('/')
+def index():
+    def g():
+        for i in range(4):
+            time.sleep(1)  # an artificial delay
+            yield i
+    return Response(stream_template('index.html', data=g()))
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=23423)
+# Adapted from https://stackoverflow.com/questions/31948285/display-data-streamed-from-a-flask-view-as-it-updates
+"""
 from time import sleep
 
 from flask import Flask, render_template
@@ -24,3 +53,4 @@ def stream():
     return app.response_class(generate(), mimetype='text/plain')
 
 app.run()
+"""
---
+++ Flask-App/loopTest.py
@@ -0,0 +1,5 @@
+for i in range(-1, 3):
+    i = i +1
+    for j in range(3):
+        j = j + 1
+        print(i, " : ", j)
