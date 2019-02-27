7052867cee278103e004cd93daa7824912dfd71e
Kevin <Kevin@DESKTOP-12A507O>
2019-02-27 15:13:38 +0000

    sarsa changed

--- FlaskApp/loopTest.py
+++ FlaskApp/loopTest.py
@@ -54,7 +54,7 @@ class State:
 
 def init():
 
-    test1 = pd.DataFrame()
+
 
     completeName = os.path.join('static/Data', 'test.txt')
 
@@ -172,75 +172,128 @@ def init():
 
     q_learning_list = []
     sarsa_list = []
+    if algorithm_form == "q-learning":
+        for e in range(N_EPISODES):
+           # print(alpha)
+            total_reward = 0
+            eps *= epsilon_decay
+            alpha = alphas[e]
+            alpha *= alphaDecay
+            state = start_state
 
-    for e in range(N_EPISODES):
-       # print(alpha)
-        total_reward = 0
-        eps *= epsilon_decay
-        alpha = alphas[e]
-        alpha *= alphaDecay
-        state = start_state
-
-        for _ in range(MAX_EPISODE_STEPS):
-
-
-            number_of_steps = 0
-            action = choose_action(state)
-            (next_state, reward, done) = act(state, action)
-            total_reward += reward
-            if algorithm_form == "q-learning":
-                print("q-learning chosen")
-                q(state)[action] = q(state, action) + alpha * (reward + gamma * np.max(q(next_state, action)) - q(state, action))
-
-            if algorithm_form == "sarsa":
-                print("sarsa chosen")
-           #Sarsa to be added
-                q(state)[action] = q(state, action) + alpha * (reward + gamma * q(next_state, action) - q(state,action))
-            state = next_state
-            number_of_steps += _
-
-            # print(action, state, "step number->", number_of_steps +1)
-
-            if number_of_steps + 1 == MAX_EPISODE_STEPS:
-                f.write('%d,' % state.agent_pos[0])
-                f.write('%d,' % state.agent_pos[1])
-
-            # To remove a row with all zero values in a dataframe
-            # Every terminal state was adding a new row with all zero  values
-            # Adapted from https://stackoverflow.com/questions/20490274/how-to-reset-index-in-a-pandas-data-frame
-            # Adapted from https://stackoverflow.com/questions/22649693/drop-rows-with-all-zeros-in-pandas-data-frame
-
-            empty_keys = {k: v for k, v in q_table.items() if sum(v) == 0}
-            for k in empty_keys:
-                del q_table[k]
-
-            with open('static/Data/Dataframe.csv', 'a',newline='') as f1:
-                 f1.write(pd.DataFrame(list(q_table.values())).to_csv(header=None))
+            for _ in range(MAX_EPISODE_STEPS):
 
 
-            if done:
-                break
+                number_of_steps = 0
+                action = choose_action(state)
+                (next_state, reward, done) = act(state, action)
+                total_reward += reward
 
-        if algorithm_form == "q-learning":
+                best_next_action = np.argmax(q(next_state))
+                td_target = reward + gamma * q(next_state)[best_next_action]
+                td_delta = td_target - q(state)[action]
+                q(state)[action] += alpha * td_delta
+               # q(state)[action] += alpha * reward + gamma * np.max(q(next_state, action) - q(state, action))
+
+                number_of_steps += _
+                state = next_state
+
+                if number_of_steps + 1 == MAX_EPISODE_STEPS:
+                    f.write('%d,' % state.agent_pos[0])
+                    f.write('%d,' % state.agent_pos[1])
+
+                # To remove a row with all zero values in a dataframe
+                # Every terminal state was adding a new row with all zero  values
+                # Adapted from https://stackoverflow.com/questions/20490274/how-to-reset-index-in-a-pandas-data-frame
+                # Adapted from https://stackoverflow.com/questions/22649693/drop-rows-with-all-zeros-in-pandas-data-frame
+
+                empty_keys = {k: v for k, v in q_table.items() if sum(v) == 0}
+                for k in empty_keys:
+                    del q_table[k]
+
+                with open('static/Data/Dataframe.csv', 'a',newline='') as f1:
+                     f1.write(pd.DataFrame(list(q_table.values())).to_csv(header=None))
+
+
+                if done:
+                    break
+
             with open('static/Data/q_learning.json', 'w') as al:
 
                 q_learning_list.append(total_reward)
                 df = pd.DataFrame(q_learning_list)
                 al.write(df.to_json())
 
-        elif algorithm_form == "sarsa":
-           with open('static/Data/sarsa.json', 'w') as al:
+            print(e)
+
+        # print(f"Episode {e + 1}: total reward -> {total_reward}")
+
+        f.close()
+
+    if algorithm_form == "sarsa":
+        for e in range(N_EPISODES):
+            # print(alpha)
+            total_reward = 0
+            eps *= epsilon_decay
+            alpha = alphas[e]
+            alpha *= alphaDecay
+            state = start_state
+            action = choose_action(state)
+            for _ in range(MAX_EPISODE_STEPS):
+
+
+                number_of_steps = 0
+
+                (next_state, reward, done) = act(state, action)
+                next_action = choose_action(next_state)
+                total_reward += reward
+
+                number_of_steps += _
+
+                    #Sarsa to be added
+
+                td_target = reward + gamma * q(next_state)[next_action]
+                td_delta = td_target - q(state)[action]
+                q(state)[action] += alpha * td_delta
+                #q(state)[action] += alpha * reward + gamma * q(next_state, action) - q(state,action)
+
+                number_of_steps += _
+                action = next_action
+                state = next_state
+                # print(action, state, "step number->", number_of_steps +1)
+
+                if number_of_steps + 1 == MAX_EPISODE_STEPS:
+                    f.write('%d,' % state.agent_pos[0])
+                    f.write('%d,' % state.agent_pos[1])
+
+                # To remove a row with all zero values in a dataframe
+                # Every terminal state was adding a new row with all zero  values
+                # Adapted from https://stackoverflow.com/questions/20490274/how-to-reset-index-in-a-pandas-data-frame
+                # Adapted from https://stackoverflow.com/questions/22649693/drop-rows-with-all-zeros-in-pandas-data-frame
+
+                empty_keys = {k: v for k, v in q_table.items() if sum(v) == 0}
+                for k in empty_keys:
+                    del q_table[k]
+
+                with open('static/Data/Dataframe.csv', 'a',newline='') as f1:
+                    f1.write(pd.DataFrame(list(q_table.values())).to_csv(header=None))
+
+
+                if done:
+                    break
+
+
+            with open('static/Data/sarsa.json', 'w') as al:
                 sarsa_list.append(total_reward)
                 df = pd.DataFrame(sarsa_list)
                 al.write(df.to_json())
 
 
-        #print(e)
+            print(e)
 
-    # print(f"Episode {e + 1}: total reward -> {total_reward}")
+        # print(f"Episode {e + 1}: total reward -> {total_reward}")
 
-    f.close()
-
+        f.close()
 
 
 
