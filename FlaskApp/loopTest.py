58e8fa95591bd20a4c88e271664a29556315ebef
Kevin <Kevin@DESKTOP-12A507O>
2019-03-01 02:41:38 +0000

    method added for epsilon and gamma decay

--- FlaskApp/loopTest.py
+++ FlaskApp/loopTest.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 from copy import deepcopy
 
 
@@ -101,28 +101,32 @@ def init():
         if grid_item == TRAP:
             reward = -100
             is_done = True
+            goal_reached = False
             f.write('%d,' % start_state.agent_pos[0])
             f.write('%d,' % start_state.agent_pos[1])
             new_grid[p[0]][p[1]] += AGENT
         elif grid_item == GOAL:
             reward = int(goal_reward)
             is_done = True
+            goal_reached = True
             f.write('%d,' % start_state.agent_pos[0])
             f.write('%d,' % start_state.agent_pos[1])
             new_grid[p[0]][p[1]] += AGENT
         elif grid_item == EMPTY:
             reward = float(per_step_cost)
             is_done = False
+            goal_reached = False
             old = state.agent_pos
             new_grid[old[0]][old[1]] = EMPTY
             new_grid[p[0]][p[1]] = AGENT
         elif grid_item == AGENT:
             reward = float(per_step_cost)
             is_done = False
+            goal_reached = False
         else:
             raise ValueError('Unknown grid item {grid_item}')
 
-        return (State(grid=new_grid, agent_pos=p), reward, is_done)
+        return (State(grid=new_grid, agent_pos=p), reward, is_done, goal_reached)
 
 
 
@@ -163,6 +167,8 @@ def init():
         return q_table[state][action]
 
     def choose_action(state):
+
+        random.seed(datetime.now())
         if random.uniform(0, 1) < eps:
             return random.choice(ACTIONS)
         else:
@@ -171,101 +177,45 @@ def init():
     if os.path.exists('static/Data/Dataframe.csv'):
         os.remove('static/Data/Dataframe.csv')
 
+    def check(gr,eps,alpha):
+        if gr:
+            if eps >0:
+                eps *= epsilon_decay
+            else:
+                eps = 0
+            if alpha >0:
+                alpha *= alphaDecay
+            else:
+                alpha = 0
+        return eps, alpha
+
     q_learning_list = []
     sarsa_list = []
     if algorithm_form == "q-learning":
         for e in range(N_EPISODES):
 
             total_reward = 0
-            eps *= epsilon_decay
-            alpha = alphas[e]
-            alpha *= alphaDecay
-            state = start_state
 
+            state = start_state
+            number_of_steps = 0
             for _ in range(MAX_EPISODE_STEPS):
 
-
-                number_of_steps = 0
 
                 action = choose_action(state)
-                (next_state, reward, done) = act(state, action)
-                total_reward += reward
+                (next_state, reward, done, goal_reached) = act(state, action)
 
+                eps, alpha = check(goal_reached,eps,alpha)
+                #print(alpha)
                 max_next_action = np.max(q(next_state))
                 target = reward + gamma * max_next_action
-                end_eq = target - q(state,action)
+                end_eq = target - q(state)[action]
                 q(state)[action] = q(state)[action] + alpha * end_eq
-
-                state = next_state
-
-
-
-                number_of_steps += _
-
-
-                if number_of_steps + 1 == MAX_EPISODE_STEPS:
-                    f.write('%d,' % state.agent_pos[0])
-                    f.write('%d,' % state.agent_pos[1])
-
-                # To remove a row with all zero values in a dataframe
-                # Every terminal state was adding a new row with all zero  values
-                # Adapted from https://stackoverflow.com/questions/20490274/how-to-reset-index-in-a-pandas-data-frame
-                # Adapted from https://stackoverflow.com/questions/22649693/drop-rows-with-all-zeros-in-pandas-data-frame
-
-                empty_keys = {k: v for k, v in q_table.items() if sum(v) == 0}
-                for k in empty_keys:
-                    del q_table[k]
-
-                with open('static/Data/Dataframe.csv', 'a',newline='') as f1:
-                     f1.write(pd.DataFrame(list(q_table.values())).to_csv(header=None))
-
-
-                if done:
-                    break
-
-            with open('static/Data/q_learning.json', 'w') as al:
-
-                q_learning_list.append(total_reward)
-                df = pd.DataFrame(q_learning_list)
-                al.write(df.to_json())
-
-            print(e)
-
-
-
-        f.close()
-
-    if algorithm_form == "sarsa":
-        for e in range(N_EPISODES):
-            # print(alpha)
-            total_reward = 0
-            eps *= epsilon_decay
-            alpha = alphas[e]
-            alpha *= alphaDecay
-            state = start_state
-            action = choose_action(state)
-            for _ in range(MAX_EPISODE_STEPS):
-
-
-                number_of_steps = 0
-
-                (next_state, reward, done) = act(state, action)
-                next_action = choose_action(next_state)
                 total_reward += reward
-
-                number_of_steps += _
-
-
-
-                target = reward + gamma * q(next_state, next_action)
-                eq_end = target - q(state,action)
-                q(state)[action] = q(state)[action] + alpha * eq_end
-                action = next_action
                 state = next_state
 
 
-                number_of_steps += _
 
+                number_of_steps += 1
 
 
                 if number_of_steps + 1 == MAX_EPISODE_STEPS:
@@ -288,15 +238,69 @@ def init():
                 if done:
                     break
 
+            with open('static/Data/q_learning.json', 'w') as al:
+
+                q_learning_list.append(total_reward)
+                df = pd.DataFrame(q_learning_list)
+                al.write(df.to_json())
+
+            print(e)
+
+
+
+
+
+    if algorithm_form == "sarsa":
+
+
+        for e in range(N_EPISODES):
+            state = start_state
+            action = choose_action(state)
+            # print(alpha)
+            total_reward = 0
+            number_of_steps = 0
+
+
+            for _ in range(MAX_EPISODE_STEPS):
+
+                (next_state, reward, done,goal_reached) = act(state, action)
+                next_action = choose_action(next_state)
+                eps, alpha = check(goal_reached,eps,alpha)
+               # print(eps)
+                target = reward + gamma * q(next_state, next_action)
+                eq_end = target - q(state)[action]
+                q(state)[action] = q(state)[action] + alpha * eq_end
+
+                number_of_steps += 1
+                if number_of_steps + 1 == MAX_EPISODE_STEPS:
+                    f.write('%d,' % state.agent_pos[0])
+                    f.write('%d,' % state.agent_pos[1])
+
+                # To remove a row with all zero values in a dataframe
+                # Every terminal state was adding a new row with all zero  values
+                # Adapted from https://stackoverflow.com/questions/20490274/how-to-reset-index-in-a-pandas-data-frame
+                # Adapted from https://stackoverflow.com/questions/22649693/drop-rows-with-all-zeros-in-pandas-data-frame
+
+                empty_keys = {k: v for k, v in q_table.items() if sum(v) == 0}
+                for k in empty_keys:
+                    del q_table[k]
+
+                with open('static/Data/Dataframe.csv', 'a',newline='') as f1:
+                    f1.write(pd.DataFrame(list(q_table.values())).to_csv(header=None))
+
+                total_reward += reward
+                action = next_action
+
+                state = next_state
+                if done:
+                    break
+
 
             with open('static/Data/sarsa.json', 'w') as al:
                 sarsa_list.append(total_reward)
                 df = pd.DataFrame(sarsa_list)
                 al.write(df.to_json())
 
-
             print(e)
 
-
-
-        f.close()
+    f.close()
