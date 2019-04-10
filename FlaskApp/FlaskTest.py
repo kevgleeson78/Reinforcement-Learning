af1395d50ef3f0907702c29c94840d92777a7473
Kevin <Kevin@DESKTOP-12A507O>
2019-04-10 18:38:40 +0100

    comments added to FlaskTest.py

--- FlaskApp/FlaskTest.py
+++ FlaskApp/FlaskTest.py
@@ -5,18 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
+    # Return the main home page when the route resource is requested
     return render_template('index.html')
 
-
+# The first request made once teh form has been submitted
 @app.route('/run', methods=['GET', 'POST'])
 def test():
 
     if request.method == 'GET':
-
+        # Call the init function from the environment.py script
         environment.init()
+        # Return done as the response text for the ajax request
+        # Originally for a work around of the Heroku time request limit
+        # Leaving in place as a loading page
         return 'done'
+
     if request.method == 'POST':
+        # Assign all form parameters to the environment variables
         environment.environment_form = request.form.get('grid_type')
         environment.algorithm_form = request.form.get('algorithm')
         environment.episodes_form = request.form.get('episode')
@@ -29,12 +34,14 @@ def test():
         environment.alpha_form = request.form.get('alpha')
         environment.alpha_form_decay = request.form.get('alpha_decay')
         environment.trap_reward = request.form.get('trap_reward')
+        # Show the waiting "loading page"
         return render_template('waiting.html')
-
+        # The loading page will make a post request to the success resource once it has received the done message
 
 @app.route('/success', methods=['GET', 'POST'])
 def dealy():
-
+    # Theese variables are used as templates within the result page for keeping the chosen parameters the user has
+    # chosen from the previous form submit.
     var0 = environment.environment_form
     var1 = environment.algorithm_form
     var2 = environment.epsilon_form
@@ -47,6 +54,7 @@ def dealy():
     var9 = environment.per_step_cost
     var10 = environment.goal_reward
     var11 = environment.trap_reward
+    # Databinding the varables to be on the result.html page
     return render_template("result.html",var0=var0,var1=var1,var2=var2,var3=var3,var4=var4,var5=var5,var6=var6,var7=var7,var8=var8,var9=var9,var10=var10,var11=var11)
 
 
